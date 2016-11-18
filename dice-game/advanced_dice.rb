357480ff095619cc664095a31086ff28c4a038b9
Patricia H. Chun <patriciahchun@gmail.com>
2016-11-18 15:36:56 -0800

    Ruby dice-game commit

--- dice-game/advanced_dice.rb
+++ dice-game/advanced_dice.rb
@@ -1,1 +1,30 @@
 # YOUR ADVANCED CODE, HERE
+def DiceGame()
+  puts "How many sides does your dice have? "
+  sides = gets.chomp
+
+  puts "How many times would you like to roll your pair of dice?"
+  num = gets.chomp
+  puts ""
+
+  while true
+    counter = 0
+
+    while counter < num.to_i
+      num1 = rand(sides.to_i) + 1
+      num2 = rand(sides.to_i) + 1
+
+      puts "You rolled a #{num1} and a #{num2}. Total: #{num1+num2}"
+      counter += 1
+    end
+
+    puts "\nRoll Again? (Y/N)> "
+    cont_game = gets.chomp
+    if cont_game == "N"
+      break
+    end
+    
+  end
+end
+
+DiceGame()
--- dice-game/dice.rb
+++ dice-game/dice.rb
@@ -1,1 +1,19 @@
 # YOUR CODE, HERE
+def DiceGame()
+  while true
+    num1 = rand(6)+1
+    num2 = rand(6)+1
+
+    puts "You rolled a #{num1} and a #{num2}"
+    puts "Total: #{num1+num2}"
+    puts "Press 'Enter' to roll again, 'S' to stop "
+    cont_game = gets.chomp
+
+    if cont_game == 'S'
+      break
+    end
+
+  end
+end
+
+DiceGame()
